9c770175f59b9d35b6b9c046cd0466f22ee65ff6
zjhzhz1985 <zjhzhz1985@163.com>
2018-05-29 15:19:23 +0800

    测试2

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
-  function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
+	function onRequest(request, response) {
+		var pathname =  url.parse(request.url).pathname;
+		console.log("Request for " + pathname + "received.");
+		response.writeHead(200, {
+			"Content-Type": "text/plain"
+		});
+		response.write("Hello World");
+		response.end();
+	}
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
 
 exports.start = start;
