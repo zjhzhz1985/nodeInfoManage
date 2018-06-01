9c22799817547e5b2bde69dd533535681b92ea79
zjhzhz1985 <zjhzhz1985@163.com>
2018-06-01 16:29:25 +0800

    ceshi14

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,26 @@
 var exec = require("child_process").exec;
 
-function start(response) {
+function start(response,postData) {
 	console.log("Requset handler 'start' was called.");
 
-	exec("find /", {
-		timeout: 10000,
-		maxBuffer: 20000 * 1024
-	}, function(error, stdout, stderr) {
-		response.writeHead(200, {
-			"Content-Type": "text/plain"
-		});
-		response.write(stdout);
-		response.end();
-	})
+	var body = '<html>' +
+		'<head>' +
+		'<meta http-equiv="Content-Type" content="text/html; ' +
+		'charset=UTF-8" />' +
+		'</head>' +
+		'<body>' +
+		'<form action="/upload" method="post">' +
+		'<textarea name="text" rows="20" cols="60"></textarea>' +
+		'<input type="submit" value="Submit text" />' +
+		'</form>' +
+		'</body>' +
+		'</html>';
+
+	response.writeHead(200, {
+		"Content-Type": "text/plain"
+	});
+	response.write(body);
+	response.end();
 }
 
 function upload(response) {
@@ -20,7 +28,7 @@ function upload(response) {
 	response.writeHead(200, {
 		"Content-Type": "text/plain"
 	});
-	response.write("Hello Upload");
+	response.write("you've sent:" +  postData);
 	response.end();
 }
 
--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + "received.");
 
-		// response.writeHead(200, {
-		// 	"Content-Type": "text/plain"
-		// });
-		route(handle, pathname, response);
-		// response.write(content);
-		// response.end();
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" +
+				postDataChunk + "'.");
+		});
+
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 
 	http.createServer(onRequest).listen(8888);
