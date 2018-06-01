b422ff453353e4532ef21cebe3ae5b918aed77e5
zjhzhz1985 <zjhzhz1985@163.com>
2018-06-01 13:48:00 +0800

    ceshi4

--- router.js
+++ router.js
@@ -1,5 +1,11 @@
-function route(pathname){
+function route(handle, pathname) {
 	console.log("about to route a request for " + pathname);
+	if (typeof handle[pathname] === 'function') {
+		return handle[pathname]();
+	} else {
+		console.log("No request handler found for" + pathname);
+		renturn "404 Not found";
+	}
 }
 
 exports.route = route;
