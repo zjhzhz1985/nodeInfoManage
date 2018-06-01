66d5809400a153713b4aab1cdef6bc4bcc53954d
zjhzhz1985 <zjhzhz1985@163.com>
2018-06-01 14:09:27 +0800

    ceshi9

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,18 @@
-function start (){
+function start() {
 	console.log("Requset handler 'start' was called.");
+
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);
+	}
+
+	sleep(10000);
+
+
 	return "Hello Start";
 }
 
-function upload (){
+function upload() {
 	console.log("Requset handler 'upload' was called.");
 	return "Hello Upload";
 }
