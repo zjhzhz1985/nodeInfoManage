28e007348deb5c0bb75584514aa4d315dc0eda54
zjhzhz1985 <zjhzhz1985@163.com>
2018-06-04 14:30:53 +0800

    测试20

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
 var exec = require("child_process").exec;
-
 var querystring = require("querystring");
+var fs = require("fs");
 
 function start(response, postData) {
 	console.log("Requset handler 'start' was called.");
@@ -18,9 +18,9 @@ function start(response, postData) {
 		'</body>' +
 		'</html>';
 
-	// response.writeHead(200, {
-	// 	"Content-Type": "text/plain"
-	// });
+	response.writeHead(200, {
+		"Content-Type": "text/html"
+	});
 	response.write(body);
 	response.end();
 }
@@ -34,5 +34,25 @@ function upload(response, postData) {
 	response.end();
 }
 
+function show(response, postData) {
+	console.log("Requset handler 'show' was called.");
+	fs.readFile("/tmp/test.png", "binary", function(error, file) {
+		if (error) {
+			response.writeHead(500, {
+				"Content-Type": "text/plain"
+			});
+			response.write(error + "\n");
+			response.end();
+		} else {
+			response.writeHead(200, {
+				"Content-Type": "image/png"
+			});
+			response.write(file, "binary");
+			response.end();
+		}
+	});
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
